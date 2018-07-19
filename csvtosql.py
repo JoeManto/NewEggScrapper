3dee0498e7e6a9b9e881859e09acfe9f4ea840c9
Joe Manto <joemanto@wireless37-121.wireless.wmich.edu>
2018-07-19 12:14:54 -0400

    added argparse to the sql tool

--- csvtosql.py
+++ csvtosql.py
@@ -1,12 +1,22 @@
 import csv
 
-tableName = "Gpu"
+
+import argparse
+
+parser = argparse.ArgumentParser(prog='PROG')
+parser.add_argument('file', nargs='?', help='name of csv with out extension')
+parser.add_argument('tableName', nargs='?', help='name of the table to be created')
+
+args = parser.parse_args()
+
+filePath = args.file +'.csv'
+tableName = args.tableName
 sqlFile = open('sqlTest.sql', 'w')
 
 def writeRowToFile(file,rowString):
 	file.write("%s\n" % rowString)
 
-with open("index1.csv",'rb') as cvsfile:
+with open(filePath,'rb') as cvsfile:
 	rows = csv.reader(cvsfile, delimiter= ',')
 	for row in rows:
 		queryString = 'INSERT INTO '+ tableName + "('"
